b35a6a3134f854eb07a2c88e56018deb7d675579
Пижанков Макар <mmpizhankov@edu.hse.ru>
2026-06-03 14:10:48 +0300

    Добавил класс Recipe

--- recipes.py
+++ recipes.py
@@ -25,3 +25,38 @@ class Ingredient:
         if not isinstance(other, Ingredient):
             return NotImplemented
         return self.name == other.name and self.unit == other.unit
+    
+
+class Recipe:
+    def __init__(self, title: str, ingredients=None):
+        self.title = title
+        self.ingredients = ingredients if ingredients is not None else []
+
+    def add_ingredient(self, ingredient: Ingredient):
+        for i in self.ingredients:
+            if i == ingredient:
+                i.quantity += ingredient.quantity
+                return
+        self.ingredients.append(ingredient)
+
+    @staticmethod
+    def is_valid_ratio(ratio):
+        return isinstance(ratio, (int, float)) and ratio > 0
+
+    def scale(self, ratio: float):
+        if not self.is_valid_ratio(ratio):
+            raise ValueError("Коэффициент должен быть положительным числом")
+
+        recipe = Recipe(self.title)
+
+        for i in self.ingredients:
+            recipe.add_ingredient(Ingredient(i.name, i.quantity * ratio, i.unit))
+
+        return recipe
+
+    def __len__(self):
+        return len(self.ingredients)
+
+    def __str__(self):
+        st = "; ".join(str(ingredient) for ingredient in self.ingredients)
+        return f"{self.title}. Ингредиенты: {st}"
